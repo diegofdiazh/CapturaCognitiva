010c97d67536eea1e2000a53da45fdb8f6909d4b
Diego Fernando Diaz Hernandez <diegofdiazh@javeriana.edu.co>
2020-09-06 13:13:23 -0500

    add .gitignore

---
+++ PortalNetCore/CapturaCognitiva/Data/Entities/Guide.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapturaCognitiva.Data.Entities
+{
+    public class Guide
+    {
+    }
+}
---
+++ PortalNetCore/CapturaCognitiva/Data/Entities/Image.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapturaCognitiva.Data.Entities
+{
+    public class Image
+    {
+        public int Id { get; set; }
+        public int Uaid { get; set; }
+        public string ImageBase64 { get; set; }
+    }
+}
